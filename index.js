45f21a08cfbacf97464c9159443c41ace25d470d
khanmdadil094 <khanmdadil094@gmail.com>
2024-12-25 20:01:12 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>this is an experiment of rebasing</h1>
+    <h1>i am other Dev</h1>
+    <h1>i want to get changes from other developer from other branches</h1>
+    <h1>using pull to get file in local repo</h1>
+    <h1>using pull to get file in local github repo</h1>
+</body>
+</html>
