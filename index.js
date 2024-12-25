9ada26f69e77b7381294a2c026e83ebb233a8c34
khanmdadil094 <khanmdadil094@gmail.com>
2024-12-25 20:04:38 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+const firstName="Md Adil"
+const lastName="Khan"
+console.log(firstName+" "+"lastName")
+
+
+
+for(let i=0;i<=1000;i++){
+    console.log(i+" ");
+    
+}
+
+
+
+const arr=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+for(let i=0;i<arr.length;i++){
+    if(arr[i]%2==0){
+        console.log(arr[i]);
+        
+    }
+}
+
+
+const newArr=arr.filter((item,ind)=>{
+ return  item%2===0    
+})
+console.log(newArr);
+
+
+
+console.log(arr);
+
+
+console.log("hello world");
+console.log("working with dev branch");
+
+
+
+console.log("hello world");
+console.log("working with dev branch");
