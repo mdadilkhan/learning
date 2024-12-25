e543a8c8c31873d700f2560f251669757ebfdedd
mdadilkhan <mdadilakhtar8@gmail.com>
2024-12-25 23:30:44 +0530

    learning stash

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const express = require('express')
 const app=express()
 
 
- 
+ app.use(express.json())
+ app.use(express.urlencoded({extended:true}))
+
+
+ app.get('/',(req,res)=>{
+     res.send('Hello World')
+ })
 
 
 const PORT=3000
